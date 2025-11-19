609cb73d4ccaa756369654a04185a413ba6b6ab0
ahsan.kabir <ahsan.kabir@bjitgroup.com>
2025-11-19 12:22:21 +0600

    Completed the first challenge of C#

---
+++ CompleteChallenge.cs
@@ -0,0 +1,10 @@
+using System;
+
+class CompleteChallenge
+{
+    static void Main()
+    {
+        Console.WriteLine("This is the first line.");
+        Console.Write("This is the second line.");
+    }
+}
--- HelloWorld.cs
+++ HelloWorld.cs
@@ -1,1 +1,4 @@
-Console.WriteLine("Hello World!");
+// Console.WriteLine("Hello World!");
+
+Console.WriteLine("Congratulations!");
+Console.Write("You wrote your first lines of code.");
